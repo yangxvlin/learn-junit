d7c30d363cc03c07dae0b404513f0a699a620c43
XuLinYang <xuliny@student.unimelb.edu.au>
2022-03-03 18:23:19 +0800

    starts learning

--- HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,12 +1,12 @@
-# learn-junit
-## resource
-- [testing code standard: Modern Best Practices for Testing in Java](https://phauer.com/2019/modern-best-practices-testing-java/)
-- [video course: Practical Java Unit Testing with JUnit 5](https://www.udemy.com/course/junit5-for-beginners/)
+package com.healthycoderapp;
 
-## how to generate a unit test?
-intelliJ: alt + enter yout method name ```then your test will be generated```
-## sample test method
-```java
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
     /**
      * bad: assertTrue(BMICalculator.isDietRecommended(89.0, 1.72));
      */
@@ -22,30 +22,12 @@ intelliJ: alt + enter yout method name ```then your test will be generated```
         // then
         assertTrue(recommended);
     }
-```
-## assertSomething
-### assertTrue
-```java
-    @Test
-    void should_ReturnTrue_When_DietRecommended() {
-        // given
-        double weight = 89.0;
-        double height = 1.72;
 
-        // when
-        boolean recommended = BMICalculator.isDietRecommended(weight, height);
-
-        // then
-        assertTrue(recommended);
-    }
-```
-### assertFalse
-```java
     @Test
     void should_ReturnFalse_When_DietNotRecommended() {
         // given
         double weight = 89.0;
-        double height = 1.72;
+        double height = 1.95;
 
         // when
         boolean recommended = BMICalculator.isDietRecommended(weight, height);
@@ -53,9 +35,7 @@ intelliJ: alt + enter yout method name ```then your test will be generated```
         // then
         assertFalse(recommended);
     }
-```
-### assertThrows
-```java
+
     @Test
     void should_ThrowArithmeticException_When_HeightZero() {
         // given
@@ -68,5 +48,4 @@ intelliJ: alt + enter yout method name ```then your test will be generated```
         // then
         assertThrows(ArithmeticException.class, executable);
     }
-```
-org.junit.jupiter.api.function.Executable
+}
